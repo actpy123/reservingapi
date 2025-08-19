dd184f3729f6d79444313ca4fd59c1ee4633445c
Raj Singh <prince18021@gmail.com>
2025-08-19 18:24:22 +0530

    Commit

--- backend/src/utils/number.utils.ts
+++
@@ -1,9 +0,0 @@
-export function toNumber(value: any): number {
-  const num = typeof value === 'number' ? value : parseInt(value, 10);
-
-  if (isNaN(num)) {
-    throw new Error(`Invalid number: ${value}`);
-  }
-
-  return num;
-}
